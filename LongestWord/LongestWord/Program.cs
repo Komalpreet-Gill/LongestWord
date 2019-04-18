9b3b9cd057332adc7ecd651cfb6086af08e23643
Komalpreet Gill <725450@student.tor.loc>
2019-04-18 15:27:17 -0400

    First commit

---
+++ LongestWord/LongestWord/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            Console.WriteLine("The longest word is");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
+        }
+        public string MyFunctionA(string input)
+        {
+
+            string[] words = input.Split(' ');
+            int wordArrayLength = words.Length;
+            int[] wordLength = new int[wordArrayLength];
+            int x = 0;
+
+            foreach (var word in words)
+            {
+
+            }
+
+
+
+            string currentWord = words[0];
+            string nextWord;
+            string longestWord = currentWord;
+
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+                if (nextWord.Length > longestWord.Length)
+                {
+                    longestWord = nextWord;
+                }
+                else
+                {
+                    longestWord = currentWord;
+                }
+
+            }
+            return longestWord;
+        }
+
+    }
+
+}
